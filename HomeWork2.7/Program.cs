7880da0bab43e907d84db99fc41d704009fb1b39
Malik <poseydon100500@gmail.com>
2023-07-09 23:15:04 +0500

    Добавьте файлы проекта.

---
+++ HomeWork2.7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class HomeWork
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение переменной x : ");
+        float x = float.Parse(Console.ReadLine());
+        Console.WriteLine("Введите значение переменной y : ");
+        float y = float.Parse(Console.ReadLine());
+        float result = (1 + (1 / x * x)) - (12 * x * x * y);
+        Console.WriteLine("Результат : " + result);
+        Console.ReadKey();
+    }
+}
